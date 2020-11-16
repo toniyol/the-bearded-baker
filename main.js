0d118a2aaea27c51992aa457cee1294fc43eba7b
toniyol <anthony.aimon@gmail.com>
2020-11-16 23:47:09 +0000

    update form

---
+++ main.js
@@ -0,0 +1,3 @@
+function msg() {
+    alert("Thank you for your order. We will send a confirmation email very soon with payment details. Now relax and have a coffee while waiting for our delicious bread");
+  }

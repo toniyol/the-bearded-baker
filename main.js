f938db8f369116cb3524cb5a48021238b992988b
toniyol <anthony.aimon@gmail.com>
2020-11-23 23:26:54 +0000

    modals in index.html

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function msg() {
     alert("Thank you for your order. We will send a confirmation email very soon with payment details. Now relax and have a coffee while waiting for our delicious bread");
   }
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
